f91d5af91de3e6082202e892d17635e44e471100
mnwana <mariellenwana@gmail.com>
2022-09-08 00:11:26 -0400

    create comment odel & rename files

--- models/Comment.js
+++ models/Comment.js
@@ -1,42 +1,44 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
-// create our Post model
-class Post extends Model {}
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-// create fields/columns for Post model
-Post.init(
+class Comment extends Model {}
+
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    post_url: {
+    comment_text: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isURL: true,
-      },
+        len: [1]
+      }
     },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "user",
-        key: "id",
-      },
+        model: 'user',
+        key: 'id'
+      }
     },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "post",
+    modelName: 'comment'
   }
 );
 
-module.exports = Post;
+module.exports = Comment;
