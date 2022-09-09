9afe69520bf0494c838d599f05af76a0e63fffa4
mnwana <mariellenwana@gmail.com>
2022-09-09 00:27:38 -0400

    get post api working

--- models/Post.js
+++ models/Post.js
@@ -19,9 +19,6 @@ Post.init(
     text: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        isURL: true,
-      },
     },
     user_id: {
       type: DataTypes.INTEGER,
